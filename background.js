a1c48e5fb457dabf0fef3ebac9ad7d05003645e3
Santeri <santeri.j.kaariainen@gmail.com>
2018-04-22 16:07:58 +0300

    everything looking good.

--- background.js
+++ background.js
@@ -1,80 +1,42 @@
-chrome.runtime.onInstalled.addListener(function() {
-    test = "test"
-    console.log("moikkamoi");
-});
-var wPrevTabId = -1;
-var tPrevTabId = -1;
-
 //this means toggling to some specific tab and back
 //for example you toggle "to" whatsapp "from" certain tab
 var toggled = {"to": -1, "from": -1};
 chrome.tabs.onHighlighted.addListener(function (c) {
     getTabsCurrentWindow(function (tabs) {
-        getActiveTab(function (currentTab) {
-            //a
-            //var fail = false;
-            //for (var i = 0; i < tabs.length; i++) {
-                //if (tabs[i].id == currentTab.id && wPrevTabId != -1 && !tabs[i].title.includes("WhatsApp")) {
-                    //fail = true;
-                    //break;
-                //}
-                //if (tabs[i].id == currentTab.id && tPrevTabId != -1 && !tabs[i].title.includes("Telegram")) {
-                    //fail = true;
-                    //break;
-                //}
-            //}
-            //b
-            
+        getActiveTab(function (currentTab) {   
             if (toggled.to != -1 && !currentTab.title.includes(toggled.to) && currentTab.id != toggled.from.id) {
                 toggled.to = -1;
                 toggled.from = -1;
             }
-
-
-            //if (fail) {
-                //wPrevTabId = -1;
-                //tPrevTabId = -1;
-            //}
         });
     });
 });
 chrome.commands.onCommand.addListener(function(command) {
-    console.log('Command:', command);
-    
-
     if (command.includes("move")) {
         getTabsCurrentWindow(function (tabs) {
             getActiveTab(function (currentTab) {
-                for (var i = 0; i < tabs.length; i++) {
-                    if (tabs[i].id == currentTab.id) {
-                        if (command == "moveright") {
-                            if (tabs.length > i+1) {
-                                activateTab(tabs[i+1].id);
-                            }
-                            else console.log("already at last tab")
-                        }
-                        else if (command == "moveleft") {
-                            if (i-1 >= 0) {
-                                activateTab(tabs[i-1].id);
-                            }
-                            else console.log("already at first tab")
-                        }
-                    }
+                var newIndex = currentTab.index;
+                if (command == "moveleft") {
+                    newIndex--;
+                    if (newIndex == -1) newIndex = tabs.length - 1;
+                } else if (command == "moveright") {
+                    newIndex++;
+                    if (newIndex == tabs.length) newIndex = 0;
                 }
+                activateTab(tabs[newIndex].id);
+                
             });
         });
     } else if (command.includes("toggle")) {
         getTabsCurrentWindow(function (tabs) {
             getActiveTab(function (currentTab) {
                 var app = command.substring(6);
-                            if (toggled.to != app) {
-                    for (var i = 0; i < tabs.length; i++) {
-                        if (tabs[i].title.includes(app) && tabs[i].id != currentTab.id) {
-                            activateTab(tabs[i].id);
-                            toggled.to = app;
-                            toggled.from = currentTab;
-                        }
-                    }
+                if (toggled.to != app) {
+                    chrome.tabs.query({windowType: "normal", currentWindow: true, title: ("*" + app + "*")}, function (tabs) {
+                        activateTab(tabs[0].id);
+                        toggled.to = app;
+                        toggled.from = currentTab;
+                    }); 
                 } else {
                     activateTab(toggled.from.id);
                     toggled.to = -1;
